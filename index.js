11749d8f7dcad564eebbbcb2357fb4ffb3b5d23f
Magen Albritton <magenelise@gmail.com>
2022-03-24 13:25:37 -0500

    commit tree 00d663f57bf6ecfbcbde9787c6e6eb7a3df76750

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+function introduction(name = "Aki") {
+    return `Hi, my name is ${name}.`;
+}
+
+
+function introductionWithLanguage(name = "Aki", language = "Ember") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name = "Gracie", language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+
